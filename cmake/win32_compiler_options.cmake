ebc3bf4489ccdac22ef28992d7af6aa7469c411c
Mateusz Łoskot <mateusz.loskot@cadcorp.com>
2021-02-05 15:30:01 +0000

    Link against crypt32 normaliz wldap32 ws2_32 - following update to cURL 7.75.0 or later
    (cherry picked from cadcorp/2.1-maintenance branch commit d43b9eaea35b8a3b1abf8ea5a2415951057ccbab)

--- cmake/win32_compiler_options.cmake
+++ cmake/win32_compiler_options.cmake
@@ -61,7 +61,10 @@ endfunction()
 #
 # Windows htonl and similar are in winsock :(
 #
-set(WINSOCK_LIBRARY ws2_32)
+# <Cadcorp>
+#set(WINSOCK_LIBRARY ws2_32)
+set(WINSOCK_LIBRARY crypt32 normaliz wldap32 ws2_32)
+# </Cadcorp>
 
 IF(DEFINED ENV{OSGEO4W_HOME})
 	set(CMAKE_INCLUDE_PATH "c:/OSGeo4W64/include;$ENV{CMAKE_INCLUDE_PATH}")
